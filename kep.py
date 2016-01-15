7d4ea43f5a45f1ebd70dd11b35c56bfa53cda990
chernals <cedric.hernalsteens@cern.ch>
2016-01-15 01:33:25 +0100

    renaming vrl to kep

--- kep.py
+++ kep.py
@@ -7,7 +7,7 @@ import varilog.converters.matlab
 
 @click.group()
 @click.option('--debug/--no-debug', default=False, help='Set the Verbosity of the output')
-@click.option('--username', prompt=True, required=True,
+@click.option('--username', required=True,
               default=lambda: getpass.getuser(), 
               help='Will default to the current username')
 @click.pass_context
@@ -24,13 +24,18 @@ def info():
     
     This will connect to Cassandra and provide summary information.
     """
-    cluster = Cluster()
+    cluster = Cluster(['188.184.77.145'])
     session = cluster.connect('varilog')
     rows = session.execute("""
     SELECT COUNT(*) FROM md_info
     """)
     for r in rows:
-        print(r)
+        print("Declared MD's: %d" % r)
+    rows = session.execute("""
+    SELECT COUNT(*) FROM md_data
+    """)
+    for r in rows:
+        print("MD cycles stored: %d" % r)
     pass
     
 @cli.group()
@@ -67,60 +72,18 @@ def create(ctx, name, comment):
         click.echo("MD created at %s" % stamp)
     else:
         click.echo("This MD already exists!")
-    
-@md.command()
-@click.argument('name', required=True)
-@click.argument('tag', required=True)
-@click.option('--comment', prompt=True, required=True)
-#@click.confirmation_option(help='Are you sure you want to add a new tag?')
-@click.pass_context
-def tag(ctx, name, tag, comment):
-    """Add a tag to an existing MD.
-    
-    Find the corresponding MD by name.
-    If found, check if the tag already exists,
-    if not, create a new tag with a comment and
-    add time information.
-    """
-    cluster = Cluster()
-    session = cluster.connect('varilog')
-    rows = session.execute("""
-    SELECT name FROM md_info WHERE name = %s
-    """, (name,))
-    md_exists = False
-    if len(rows.current_rows) is not 0:
-        md_exists = True
-    if not md_exists:
-        click.echo("This MD does not exist, 'create' it first.")
-        exit()
-        
-    rows = session.execute("""
-    SELECT name FROM md_info WHERE name = %s and tag = %s
-    """, (name, tag))
-    tag_exists = False
-    if len(rows.current_rows) is not 0:
-        tag_exists = True
-    if tag_exists:
-        click.echo("This tag already exists!")
-        exit()
-    stamp = datetime.now()
-    session.execute("""
-    INSERT INTO md_info(name, tag, tag_comment, tag_created, cycle) VALUES(%s, %s, %s, %s, NOW())
-    """, (name, tag, comment, stamp))
         
 @md.command()
 @click.argument('name')
 @click.argument('tag')
 @click.argument('path')
-@click.option('--comment', prompt=True, required=True)
 @click.option('--format', default='matlab')
-def push(name, tag, path, comment, format):
+def push(name, tag, path, format):
     if format is not 'matlab':
         click.echo("Only Matlab pulls are supported.")
         exit()
     if format is 'matlab':
-        conv = varilog.converters.matlab.Converter(name=name, tag=tag, comment=comment, path=path)
-    pass
+        conv = varilog.converters.matlab.Converter(name=name, tag=tag, path=path)
     
 @md.command()
 @click.option('--user')
@@ -131,14 +94,10 @@ def list(user):
     cluster = Cluster()
     session = cluster.connect('varilog')
     rows = session.execute("""
-    SELECT DISTINCT name, tag, users FROM md_data
+    SELECT name, tag, users FROM md_info
     """)
-    tags_by_md = {}
     for r in rows:
-        if tags_by_md.get(r[0]) is None:
-            tags_by_md[r[0]] = [[r[1], r[2]]]
-        else:
-            tags_by_md[r[0]].append([r[1], r[2]])
+        break
     if user is None:
         print(tags_by_md)
     else:
